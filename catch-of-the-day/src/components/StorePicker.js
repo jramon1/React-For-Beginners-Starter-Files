fcc92a311188828667a1a07186414ea8e6992bd1
Jesse Ramon Brown <jramonibz555@gmail.com>
2017-09-04 08:08:12 +0200

    setting up catch of day app

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className='store-selector'>
+          { /*HELLLOOO*/}
+          <h2> Please enter a store </h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store </button>
+      </form>
+
+      )
+  }
+
+}
+
+export default StorePicker;
