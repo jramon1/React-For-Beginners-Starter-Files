ff097fb05135cb0aae40e108dc8d864557a9ffc9
Jesse Ramon Brown <jramonibz555@gmail.com>
2017-09-11 23:33:34 +0200

    rendeing of fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './fish';
 import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
@@ -10,7 +11,7 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
-		this.state ={
+		this.state = {
 			fishes: {},
 			order: {}
 		};
@@ -36,6 +37,15 @@ class App extends React.Component {
 	    <div className="catch-of-the-day">
 	      <div className="menu">
 	      <Header tagline="Fresh Seafood Market"/>
+        <ul className="list-of-fishes" >
+          {
+            Object
+              .keys(this.state.fishes)
+              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+          }
+
+        </ul>
+        <Fish />
 	      </div>
 	      <Order />
 	      <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
