8988109ece256b4e94b9550fff4dc5b0890a8426
YouDev <jramonibz555@gmail.com>
2017-09-20 12:49:09 +0200

    add firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBcQLx-Btmi3zb9CofltzjjUfCQTB4p2b8",
+    authDomain: "catch-of-the-day-jesse-ramon.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jesse-ramon.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base.js';
 
 class App extends React.Component {
 	constructor() {
@@ -17,6 +18,18 @@ class App extends React.Component {
 			order: {}
 		};
 	}
+
+	componentWillMount() {
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+			context: this,
+			state: 'fishes'
+		});
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
+
 	addFish(fish) {
 		//update our state
 		const fishes = {...this.state.fishes};
